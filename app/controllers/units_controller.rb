c08fdb7698d895f8e034359d868754265d962587
Keyllian7 <keyllianazevedo2@gmail.com>
2025-02-13 15:40:06 -0300

    feat: adding the rest of the unit's basic functions

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -1,10 +1,15 @@
 class UnitsController < ApplicationController
+  before_action :set_unit, only: [:edit, :update, :show, :destroy]
   before_action :authenticate_user!
   load_and_authorize_resource
 
   def index
     @units = Unit.all
   end
+
+  def show
+  end
+
   def new
     @unit = Unit.new
   end
@@ -18,9 +23,30 @@ class UnitsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @unit.update(unit_params)
+      redirect_to units_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @unit.destroy
+    redirect_to units_path
+  end
+
   private
 
   def unit_params
     params.expect(unit: [ :name, :email ])
   end
+
+  def set_unit
+    @unit = Unit.find(params[:id])
+  end
+
 end
