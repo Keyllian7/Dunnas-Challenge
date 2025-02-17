0732b9d38cf78c4b21e95a0bca711b23d0c2c191
Keyllian7 <keyllianazevedo2@gmail.com>
2025-02-17 15:22:58 -0300

    feat: adjusting permissions, view, seed and creating information on the dashboard

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,5 +12,10 @@ user = User.where(email: "keyllianazevedo2@gmail.com").first_or_initialize
 user.update!(
   password: "123456",
   password_confirmation: "123456",
-  role: "admin"
+  confirmed_at: Time.now,
+  role: 2,
+  cpf: "24327784052", # CPF gerado pelo site https://www.4devs.com.br/gerador_de_cpf
+  rg: "123456789",
+  name: "Keyllian Azevedo",
+  telephone: "55 92 36170584", # Telefone gerado pelo site https://geradornv.com.br/gerador-telefone/
 )
