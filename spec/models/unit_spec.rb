048f8c33232cf12305da55f8ae30f793f50b032a
Keyllian7 <keyllianazevedo2@gmail.com>
2025-02-25 23:00:39 -0300

    test: adding association tests in the unit model

--- spec/models/unit_spec.rb
+++ spec/models/unit_spec.rb
@@ -7,4 +7,8 @@ RSpec.describe Unit, type: :model do
     it { should validate_uniqueness_of(:name) }
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
+
+    it { should have_many(:sectors) }
+    it { should have_many(:visits) }
+    it { should have_many(:users) }
 end
